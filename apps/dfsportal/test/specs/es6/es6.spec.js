98028839650970a599b149efb0f8ba255ef8e441
Carvalho <rogerio_carvalho@amer.dell.com>
2016-04-26 07:57:35 -0300

    Added Karma initial setup with support to ES6.

--- apps/dfsportal/test/specs/es6/es6.spec.js
+++ apps/dfsportal/test/specs/es6/es6.spec.js
@@ -1,11 +1,7 @@
 'use strict';
 
-const PageFactory = require('../../features/support/page-factory');
-
 describe('Page Factory', () => {
 	it('should', () => {
-		const pageFactory = new PageFactory({}, {});
-		const page = pageFactory.create('signin');
-		page.title.should.be.eq('Sign In | Dell Financial Services');
+		expect(true).toEqual(true);
 	})
 });
